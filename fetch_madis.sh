bab3330845bd56e00d637587a92553d185e42d6f
kellyclose-lrewater <kelly.close@LREWater.com>
2011-05-16 12:10:07 -0600

    first test script for pulling down MADIS data with a shell script

---
+++ fetch_madis.sh
@@ -0,0 +1,2 @@
+php udfcd_php_xml_madis_1H.php > /var/www/UDFCD/hydromet/services/madis_fetch_1h_log.html 2>&1
+php udfcd_php_xml_madis_24H.php > /var/www/UDFCD/hydromet/services/madis_fetch_24h_log.html 2>&1
