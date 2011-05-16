4f22696d8fe5271d7b10c16cc950e977d06c73b8
kellyclose-lrewater <kelly.close@LREWater.com>
2011-05-16 13:58:33 -0600

    Current working versions of MADIS fetching scripts

--- udfcd_php_xml_madis_1h.php
+++ udfcd_php_xml_madis_1h.php
@@ -7,7 +7,7 @@
  * get the data from the MADIS server
  */
   echo 'Beginning the PHP script that accesses and processes an XML data stream from the NOAA MADIS server<br />';
-  echo 'Processing began at '.date("F j, Y, g:i a").'  Pulling back 75 minutes.<br />';
+  echo 'Processing began at '.date("F j, Y, g:i:s a").'  Pulling MADIS data from prior 65 minutes.<br />';
     /*
      *  define the XML source URL
      *  the password in this url has to be changed every 60 days.
@@ -18,39 +18,30 @@
      *  "What was the first street you lived on?"  College
      *  "What was the first school you attended?"  Stevensville
      */
-//      $URL = 'https://lrcwe_madis_public:Fr4kUHuzaTuc@madis-data.noaa.gov/madisPublic/cgi-bin/madisXmlPublicDir?rdr=&amp;time=0&amp;minbck=-60&amp;minfwd=0&amp;recwin=4&amp;dfltrsel=2&amp;state=CO&amp;latll=37.0&amp;lonll=-109.0&amp;latur=41.0&amp;lonur=-102.0&amp;stanam=&amp;stasel=0&amp;pvdrsel=0&amp;varsel=2&amp;qcsel=0&amp;xml=1&amp;csvmiss=0';
       $URLbase = 'https://lrcwe_madis_public:Fr4kUHuzaTuc@madis-data.noaa.gov/madisPublic/cgi-bin/madisXmlPublicDir?';
-      $URLbase .= 'rdr=&amp;time=0&amp;minbck=-75&amp;minfwd=0&amp;recwin=4&amp;dfltrsel=2&amp;';
+      $URLbase .= 'rdr=&amp;time=0&amp;minbck=-65&amp;minfwd=0&amp;recwin=4&amp;dfltrsel=2&amp;';
       $URLbase .= 'state=CO&amp;latll=37.0&amp;lonll=-109.0&amp;latur=41.0&amp;lonur=-102.0&amp;';
       $URLbase .= 'stanam=&amp;stasel=0&amp;pvdrsel=0&amp;varsel=1&amp;qcsel=0&amp;xml=1&amp;csvmiss=0&amp;';
 // Meteorological Precip Variables
       $URLpcp1h = 'nvars=PCP1H';
       $URLhydro = 'nvars=PCP1H&amp;dataset=hydro';
-//      $URLpcp24h = 'nvars=PCP24H&amp;&amp;nvars=SNOW24H';
 // Wind Variables
       $URLwind = 'nvars=FF&amp;nvars=FFGUST';
-//      $URLwind = 'nvars=DD&amp;nvars=FF&amp;nvars=DDMAX1H';
-//      $URLwindmax = 'nvars=DDMAX&amp;nvars=FFGUST&amp;nvars=DDGUST';
 // Temperature Variables
       $URLtemp = 'nvars=TD&amp;nvars=T';
-//      $URLtemp24h = 'nvars=T24MIN&amp;nvars=T24MAX&amp;nvars=T24MINT&amp;nvars=T24MAXT';
 // Other Variables
       $URLrh = 'nvars=RH';
       $URLfuel = 'nvars=FUELM&amp;nvars=FUELT';
-
-//      echo '  URL = '.$URLbase.$URLpcp1h.'<br />';
-//Just Station Names
-//view-source:https://madis-data.noaa.gov/madisPublic/cgi-bin/madisXmlPublicDir?rdr=&time=0&minbck=-60&minfwd=0&recwin=3&dfltrsel=2&state=CO&latll=0.0&lonll=0.0&latur=90.0&lonur=0.0&stanam=&stasel=0&pvdrsel=0&varsel=1&qcsel=1&xml=1&csvmiss=0&nvars=STALOC
-    /*
-     *  open the XML source
-     */
+// echo '  URL = '.$URLbase.$URLpcp1h.'<br />';
+// Count total records over all loops
 $counter1=0;
-    echo '<br />Processing 1 Hour Precip (PCP1H), '.date("F j, Y, g:i a").'<br />';
+    echo '<br />Processing 1 Hour Precip (PCP1H).<br />';
       $xmlstring1 = file_get_contents($URLbase.$URLpcp1h);
       if( ! $xml = simplexml_load_string($xmlstring1) ) {
         echo '<b>***Error***.  Unable to load 1 Hour Precip XML.</b><br />'; 
       } else {
-        echo '   Inserting 1 Hour Precip, '.date("F j, Y, g:i a").'<br />';
+        echo '    Inserting Records from XML, BEGIN: '.date("F j, Y, g:i:s a").'<br />';
+        // Count XML records in this loop
         $counter2=0;
         foreach( $xml as $response ) {
           ++$counter1;
@@ -78,16 +69,18 @@ $counter1=0;
           $query .= "','";
           $query .= $response->attributes()->QCR;
           $query .= "')";
-          $pgresult = pg_exec($dbhandle, $query);
+          $pgresult = pg_send_query($dbhandle, $query);
+          $res1=pg_get_result($dbhandle);
        } 
-    echo '  ...1 Hour Precip done! '.$counter2.' records inserted. '.date("F j, Y, g:i a").'<br />';
+    echo '    ...Finished.  '.$counter2.' records inserted. END:  '.date("F j, Y, g:i:s a").'<br />';
       }
-    echo '<br />Processing 1 Hour Precip from UDFCD (hydro param), '.date("F j, Y, g:i a").'<br />';
+    echo '<br />Processing 1 Hour Precip from UDFCD (hydro param).<br />';
       $xmlstring2 = file_get_contents($URLbase.$URLhydro);
       if( ! $xml = simplexml_load_string($xmlstring2) ) {
         echo '<b>***Error***.  Unable to load 1 Hour UDFCD (hydro) Precip XML.</b><br />'; 
       } else {
-        echo '   Inserting 1 Hour UDFCD Precip, '.date("F j, Y, g:i a").'<br />';
+        echo '    Inserting Records from XML, BEGIN: '.date("F j, Y, g:i:s a").'<br />';
+        // Count XML records in this loop
         $counter2=0;
         foreach( $xml as $response ) {
           ++$counter1;
@@ -115,16 +108,18 @@ $counter1=0;
           $query .= "','";
           $query .= $response->attributes()->QCR;
           $query .= "')";
-          $pgresult = pg_exec($dbhandle, $query);
+          $pgresult = pg_send_query($dbhandle, $query);
+          $res1=pg_get_result($dbhandle);
       } 
-    echo '  ...1 Hour UDFCD Precip done! '.$counter2.' records inserted. '.date("F j, Y, g:i a").'<br />';
+    echo '    ...Finished.  '.$counter2.' records inserted. END:  '.date("F j, Y, g:i:s a").'<br />';
       }
-    echo '<br />Processing Wind (FF, FFGUST), '.date("F j, Y, g:i a").'<br />';
+    echo '<br />Processing Wind (FF, FFGUST).<br />';
       $xmlstring3 = file_get_contents($URLbase.$URLwind);
       if( ! $xml = simplexml_load_string($xmlstring3) ) {
         echo '<b>***Error***.  Unable to load Wind XML.</b><br />'; 
       } else {
-        echo '   Inserting Wind, '.date("F j, Y, g:i a").'<br />';
+        echo '    Inserting Records from XML, BEGIN: '.date("F j, Y, g:i:s a").'<br />';
+        // Count XML records in this loop
         $counter2=0;
         foreach( $xml as $response ) {
           ++$counter1;
@@ -152,16 +147,18 @@ $counter1=0;
           $query .= "','";
           $query .= $response->attributes()->QCR;
           $query .= "')";
-          $pgresult = pg_exec($dbhandle, $query);
+          $pgresult = pg_send_query($dbhandle, $query);
+          $res1=pg_get_result($dbhandle);
       } 
-    echo '  ...Wind done! '.$counter2.' records inserted. '.date("F j, Y, g:i a").'<br />';
+    echo '    ...Finished.  '.$counter2.' records inserted. END:  '.date("F j, Y, g:i:s a").'<br />';
       }
-    echo '<br />Processing Temp (TD, T), '.date("F j, Y, g:i a").'<br />';
+    echo '<br />Processing Temp (TD, T).<br />';
       $xmlstring5 = file_get_contents($URLbase.$URLtemp);
       if( ! $xml = simplexml_load_string($xmlstring5) ) {
         echo '<b>***Error***.  Unable to load Temp XML.</b><br />'; 
       } else {
-        echo '   Inserting Temp, '.date("F j, Y, g:i a").'<br />';
+        echo '    Inserting Records from XML, BEGIN: '.date("F j, Y, g:i:s a").'<br />';
+        // Count XML records in this loop
         $counter2=0;
         foreach( $xml as $response ) {
           ++$counter1;
@@ -189,16 +186,18 @@ $counter1=0;
           $query .= "','";
           $query .= $response->attributes()->QCR;
           $query .= "')";
-          $pgresult = pg_exec($dbhandle, $query);
+          $pgresult = pg_send_query($dbhandle, $query);
+          $res1=pg_get_result($dbhandle);
       } 
-    echo '  ...Temp done! '.$counter2.' records inserted. '.date("F j, Y, g:i a").'<br />';
+    echo '    ...Finished.  '.$counter2.' records inserted. END:  '.date("F j, Y, g:i:s a").'<br />';
       }
-    echo '<br />Processing Relative Humidity (RH), '.date("F j, Y, g:i a").'<br />';
+    echo '<br />Processing Relative Humidity (RH).<br />';
       $xmlstring7 = file_get_contents($URLbase.$URLrh);
       if( ! $xml = simplexml_load_string($xmlstring7) ) {
         echo '<b>***Error***.  Unable to load Relative Humidity XML.</b><br />'; 
       } else {
-        echo '   Inserting Relative Humidity, '.date("F j, Y, g:i a").'<br />';
+        echo '    Inserting Records from XML, BEGIN: '.date("F j, Y, g:i:s a").'<br />';
+        // Count XML records in this loop
         $counter2=0;
         foreach( $xml as $response ) {
           ++$counter1;
@@ -226,17 +225,18 @@ $counter1=0;
           $query .= "','";
           $query .= $response->attributes()->QCR;
           $query .= "')";
-//          echo $query.'<br />';
-          $pgresult = pg_exec($dbhandle, $query);
+          $pgresult = pg_send_query($dbhandle, $query);
+          $res1=pg_get_result($dbhandle);
       } 
-    echo '  ...Relative Humidity done! '.$counter2.' records inserted. '.date("F j, Y, g:i a").'<br />';
+    echo '    ...Finished.  '.$counter2.' records inserted. END:  '.date("F j, Y, g:i:s a").'<br />';
       }
-    echo '<br />Processing Fuel (FUELM, FUELT), '.date("F j, Y, g:i a").'<br />';
+    echo '<br />Processing Fuel (FUELM, FUELT).<br />';
       $xmlstring8 = file_get_contents($URLbase.$URLfuel);
       if( ! $xml = simplexml_load_string($xmlstring8) ) {
         echo '<b>***Error***.  Unable to load Fuel XML.</b><br />'; 
       } else {
-        echo '   Inserting Fuel, '.date("F j, Y, g:i a").'<br />';
+        echo '    Inserting Records from XML, BEGIN: '.date("F j, Y, g:i:s a").'<br />';
+        // Count XML records in this loop
         $counter2=0;
         foreach( $xml as $response ) {
           ++$counter1;
@@ -264,18 +264,18 @@ $counter1=0;
           $query .= "','";
           $query .= $response->attributes()->QCR;
           $query .= "')";
-//          echo $query.'<br />';
-          $pgresult = pg_exec($dbhandle, $query);
+          $pgresult = pg_send_query($dbhandle, $query);
+          $res1=pg_get_result($dbhandle);
       } 
-    echo '  ...Fuel done! '.$counter2.' records inserted. '.date("F j, Y, g:i a").'<br />';
+    echo '    ...Finished.  '.$counter2.' records inserted. END:  '.date("F j, Y, g:i:s a").'<br />';
       }
   //call post processing code:
-  echo 'Calling pg function data_processing.madis_hourly_process()<br />';
+  echo 'Calling pg function data_processing.madis_hourly_process(). '.date("F j, Y, g:i:s a").'<br />';
   $query = 'SELECT * FROM data_processing.madis_hourly_process()';
-  $pgresult = pg_exec($dbhandle, $query);
-  echo 'Calling pg function data_processing.madis_hourly_purge()<br />';
+  $pgresult = pg_send_query($dbhandle, $query);
+  echo 'Calling pg function data_processing.madis_hourly_purge(). '.date("F j, Y, g:i:s a").'<br />';
   $query = 'SELECT * FROM data_processing.madis_hourly_purge()';
-  $pgresult = pg_exec($dbhandle, $query);
+  $pgresult = pg_send_query($dbhandle, $query);
  
-  echo 'Processing ended at '.date("F j, Y, g:i a").'. '.$counter1.' total records inserted.';
+  echo 'Processing ended at '.date("F j, Y, g:i:s a").'. '.$counter1.' total records inserted.';
 ?>
--- udfcd_php_xml_madis_24h.php
+++ udfcd_php_xml_madis_24h.php
@@ -7,7 +7,7 @@
  * get the data from the MADIS server
  */
   echo 'Beginning the PHP script that accesses and processes an XML data stream from the NOAA MADIS server<br />';
-  echo 'Processing began at '.date("F j, Y, g:i a").'  Pulling back 3 hours minutes.<br />';
+  echo 'Processing began at '.date("F j, Y, g:i:s a").'  Pulling MADIS data from prior 3 hours (185 minutes).<br />';
     /*
      *  define the XML source URL
      *  the password in this url has to be changed every 60 days.
@@ -18,27 +18,23 @@
      *  "What was the first street you lived on?"  College
      *  "What was the first school you attended?"  Stevensville
      */
-//      $URL = 'https://lrcwe_madis_public:Fr4kUHuzaTuc@madis-data.noaa.gov/madisPublic/cgi-bin/madisXmlPublicDir?rdr=&amp;time=0&amp;minbck=-60&amp;minfwd=0&amp;recwin=4&amp;dfltrsel=2&amp;state=CO&amp;latll=37.0&amp;lonll=-109.0&amp;latur=41.0&amp;lonur=-102.0&amp;stanam=&amp;stasel=0&amp;pvdrsel=0&amp;varsel=2&amp;qcsel=0&amp;xml=1&amp;csvmiss=0';
       $URLbase = 'https://lrcwe_madis_public:Fr4kUHuzaTuc@madis-data.noaa.gov/madisPublic/cgi-bin/madisXmlPublicDir?';
-      $URLbase .= 'rdr=&amp;time=0&amp;minbck=-180&amp;minfwd=0&amp;recwin=4&amp;dfltrsel=2&amp;';
+      $URLbase .= 'rdr=&amp;time=0&amp;minbck=-185&amp;minfwd=0&amp;recwin=4&amp;dfltrsel=2&amp;';
       $URLbase .= 'state=CO&amp;latll=37.0&amp;lonll=-109.0&amp;latur=41.0&amp;lonur=-102.0&amp;';
       $URLbase .= 'stanam=&amp;stasel=0&amp;pvdrsel=0&amp;varsel=1&amp;qcsel=0&amp;xml=1&amp;csvmiss=0&amp;';
-// Meteorological Precip Variables
+// 24 Hour Precip Variables
       $URLpcp24h = 'nvars=PCP24H&amp';
       $URLhydro = 'nvars=PCP24H&amp;dataset=hydro';
-//      echo '  URL = '.$URLbase.$URLpcp1h.'<br />';
-//Just Station Names
-//view-source:https://madis-data.noaa.gov/madisPublic/cgi-bin/madisXmlPublicDir?rdr=&time=0&minbck=-60&minfwd=0&recwin=3&dfltrsel=2&state=CO&latll=0.0&lonll=0.0&latur=90.0&lonur=0.0&stanam=&stasel=0&pvdrsel=0&varsel=1&qcsel=1&xml=1&csvmiss=0&nvars=STALOC
-    /*
-     *  open the XML source
-     */
+// echo '  URL = '.$URLbase.$URLpcp24h.'<br />';
+// Count total records over all loops
 $counter1=0;
-    echo '<br />Processing 1 Hour Precip (PCP24H), '.date("F j, Y, g:i a").'<br />';
+    echo '<br />Processing 1 Hour Precip (PCP24H).<br />';
       $xmlstring1 = file_get_contents($URLbase.$URLpcp24h);
       if( ! $xml = simplexml_load_string($xmlstring1) ) {
         echo '<b>***Error***.  Unable to load 24 Hour Precip XML.</b><br />'; 
       } else {
-        echo '   Inserting 24 Hour Precip, '.date("F j, Y, g:i a").'<br />';
+        echo '    Inserting Records from XML, BEGIN: '.date("F j, Y, g:i:s a").'<br />';
+        // Count XML records in this loop
         $counter2=0;
         foreach( $xml as $response ) {
           ++$counter1;
@@ -66,16 +62,17 @@ $counter1=0;
           $query .= "','";
           $query .= $response->attributes()->QCR;
           $query .= "')";
-          $pgresult = pg_exec($dbhandle, $query);
+          $pgresult = pg_send_query($dbhandle, $query);
+          $res1=pg_get_result($dbhandle);
        } 
-    echo '  ...24 Hour Precip done! '.$counter2.' records inserted. '.date("F j, Y, g:i a").'<br />';
+       echo '    ...Finished.  '.$counter2.' records inserted. END:  '.date("F j, Y, g:i:s a").'<br />';
       }
-    echo '<br />Processing 24 Hour Precip from UDFCD (hydro param), '.date("F j, Y, g:i a").'<br />';
+    echo '<br />Processing 24 Hour Precip from UDFCD (hydro param).<br />';
       $xmlstring2 = file_get_contents($URLbase.$URLhydro);
       if( ! $xml = simplexml_load_string($xmlstring2) ) {
         echo '<b>***Error***.  Unable to load 24 Hour UDFCD (hydro) Precip XML.</b><br />'; 
       } else {
-        echo '   Inserting 24 Hour UDFCD Precip, '.date("F j, Y, g:i a").'<br />';
+        echo '    Inserting Records from XML, BEGIN: '.date("F j, Y, g:i:s a").'<br />';
         $counter2=0;
         foreach( $xml as $response ) {
           ++$counter1;
@@ -103,14 +100,16 @@ $counter1=0;
           $query .= "','";
           $query .= $response->attributes()->QCR;
           $query .= "')";
-          $pgresult = pg_exec($dbhandle, $query);
+          $pgresult = pg_send_query($dbhandle, $query);
+          $res1=pg_get_result($dbhandle);
       } 
-    echo '  ...UDFCD 24 HourPrecip done! '.$counter2.' records inserted. '.date("F j, Y, g:i a").'<br />';
+    echo '    ...Finished.  '.$counter2.' records inserted. END:  '.date("F j, Y, g:i:s a").'<br />';
       }
   //call post processing code:
-  echo 'Calling pg function data_processing.madis_daily_process()<br />';
+  echo '<br />Calling pg function data_processing.madis_daily_process(): '.date("F j, Y, g:i:s a").'<br />';
   $query = 'SELECT * FROM data_processing.madis_daily_process()';
-  $pgresult = pg_exec($dbhandle, $query);
+  $pgresult = pg_send_query($dbhandle, $query);
+  $res1=pg_get_result($dbhandle);
  
-  echo 'Processing ended at '.date("F j, Y, g:i a").'. '.$counter1.' total records inserted.';
+  echo 'Processing ended at '.date("F j, Y, g:i:s a").'. '.$counter1.' total records inserted.';
 ?>
